db99bc54df60060a5bc3e65084699c0446e72632
Sujeevanan Loganathan <lsujee2000@gmail.com>
2025-01-21 18:24:03 +0530

    get books handler setup to get the getbookresponse

--- Books.Application/Queries/Books/GetBooks/GetBooksQueryHandler.cs
+++ Books.Application/Queries/Books/GetBooks/GetBooksQueryHandler.cs
@@ -1,6 +1,8 @@
 using Books.Contracts.Responses;
 using Books.Infrastructure;
+using Mapster;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Books.Application.Queries.Books.GetBooks;
 
@@ -15,7 +17,8 @@ public class GetBooksQueryHandler:IRequestHandler<GetBooksQuery,GetBooksResponse
 
     public async Task<GetBooksResponse> Handle(GetBooksQuery request, CancellationToken cancellationToken)
     {
-        
+        var books = await _booksDbContext.Books.ToListAsync(cancellationToken);
+        return books.Adapt<GetBooksResponse>();
     }
     
 }
